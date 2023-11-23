09ba004a2c8bb05afbdf05effcc7f8216b095925
lvherrerab <l.herrerab@uniandes.edu.co>
2023-11-23 11:13:57 -0500

    Prueba de empaquetamiento

---
+++ Prueba_empaquetamiento/model/k_medias.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 23 10:23:52 2023
+
+@author: linaherrera
+"""
+
+# model/clustering_model.py
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
+import unidecode
+import re
+import spacy
+
+# Cargar el modelo NLP de Spacy
+nlp = spacy.load("es_core_news_sm")
+
+# Cargar datos
+df = pd.read_csv("https://raw.githubusercontent.com/lvherrerab/Sectorizaci-n-de-contratos-p-blicos-en-Colombia/main/OUTPUT/muestra_sincont_todas_varia.csv")
+
+# Definir la función de limpieza de texto
+def limpiar_texto(string):
+    # (Tu función de limpieza aquí)
+
+# Crear vectorizador TF-IDF
+tfidf_vectorizer = TfidfVectorizer()
+
+# Calcular la matriz TF-IDF
+tfidf_matrix = tfidf_vectorizer.fit_transform(df['objeto_del_contrato'])
+
+# Crea y entrena el modelo K-Means
+n_clusters = 24
+n_init = 10
+random_state = 123
+
+kmeans = KMeans(n_clusters=n_clusters, n_init=n_init, random_state=random_state)
+kmeans.fit(tfidf_matrix)
+
+def predict_cluster(input_text):
+    # Realizar el preprocesamiento del texto
+    preprocessed_text = limpiar_texto(input_text)
+
+    # Utilizar el vectorizador TF-IDF para convertir el texto a una matriz TF-IDF
+    input_matrix = tfidf_vectorizer.transform([preprocessed_text])
+
+    # Utilizar el modelo K-Means para predecir el cluster
+    cluster_label = kmeans.predict(input_matrix)[0]
+
+    return cluster_label
