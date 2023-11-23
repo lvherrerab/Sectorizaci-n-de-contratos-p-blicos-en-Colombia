7aa622120fe80220b27998441d4c6b97b7213a23
lvherrerab <l.herrerab@uniandes.edu.co>
2023-11-23 13:05:28 -0500

    Funcion de limpieza

--- Prueba_empaquetamiento/model/clustering_model.py
+++ Prueba_empaquetamiento/model/clustering_model.py
@@ -22,7 +22,38 @@ df = pd.read_csv("https://raw.githubusercontent.com/lvherrerab/Sectorizaci-n-de-
 
 # Definir la función de limpieza de texto
 def limpiar_texto(string):
-    # (Tu función de limpieza aquí)
+    
+    #Estandarizar acentuaciones
+    out = unidecode.unidecode(string)
+
+    #Poner en minúsculas
+    out = out.lower()
+    
+    #Eliminar caracteres que no sean del alfabeto básico latino [A-Za-z0-9_] espacios y saltos de línea
+    out = re.sub(r"[^\w\s]|\n", ' ', out)
+    
+    #Eliminar uno o varios dígitos seguidos
+    out = re.sub("\d+", "", out)
+    
+    #Eliminar todos los espacios seguidos luego de reemplazar el texto
+    out = re.sub('\s+', ' ', out)
+
+    #NLP object
+    out = nlp(out)
+    #Eliminar palabras de parada 'stopwords'
+    out = [token.text for token in out if not token.is_stop]
+    out = " ".join(out)
+
+    #Lematizar
+    lemmas =[token.lemma_ for token in nlp(out)]
+    # Convertir la lista de lemmas nuevamente a texto
+    out = " ".join(lemmas)
+
+    #Remover artículos (el/la/lo/etc.) o palabras de dos letras")
+    out = " ".join([token.text for token in nlp(out) if len(token) > 2])
+
+    return out
+    
 
 # Crear vectorizador TF-IDF
 tfidf_vectorizer = TfidfVectorizer()
